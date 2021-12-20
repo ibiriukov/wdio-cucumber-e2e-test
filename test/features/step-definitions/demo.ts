c691aada538115701810af582f257dda4f91fa51
ibiriukov <ejbiriukov@gmail.com>
2021-12-20 11:04:15 -0600

    my commit

--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -23,9 +23,40 @@ Then(/^Click on the first search result$/, async function () {
 }
 )
 
-Then(/^URL should match (.*)$/, async function(expectedURL){
+Then(/^URL should match (.*)$/, async function (expectedURL) {
     console.log('>> expectedURL: ${expectedURL}');
     let url = await browser.getUrl()
     chai.expect(url).to.equal(expectedURL)
 
-})  
+})
+
+// Web interactions
+Given(/^A web page is opened$/, async function () {
+    await browser.url('/inputs')
+    await browser.setTimeout({ implicit: 15000, pageLoad: 10000 })
+    await browser.maximizeWindow()
+
+})
+
+When(/^Perform web interactions$/, async function () {
+    /**
+     * input box
+     * Actions:
+     * 1. type into imput box
+     * 2. clear the field or add value
+     * 3. click and type
+     * 4. slow typing
+     */
+    let number = 12345
+    let strNum = number.toString()
+    let ele = await $('[type = number]')
+    await ele.click() //scrollinoview or moveto alternatives
+    // await ele.setValue(strNum)
+    for (let i = 0; i < strNum.length; i++) {
+        //await ele.setValue(strNum[i]) 
+        await browser.pause(1000)
+        await browser.keys(strNum[i])
+    }
+
+    await browser.debug()
+})
