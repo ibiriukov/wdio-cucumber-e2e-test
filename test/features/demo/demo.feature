e09a701be5aeed690136d683a22a8aa084bf4612
ibiriukov <ejbiriukov@gmail.com>
2022-01-25 10:12:08 -0600

    initial commit

--- test/features/demo/demo.feature
+++ test/features/demo/demo.feature
@@ -1,12 +1,54 @@
 Feature: Demo feature
 
-    # @demo
-    Scenario Outline: Run first demo feature
-        Given Google page is opened
-        When Search with <SearchItem>
-        Then Click on the first search result
-        Then URL should match <ExpectedURL>
+
+    Scenario Outline: Search Kasas in at least three different locations including Austin, TX
+        Given Kasa page is opened
+        When Search with <searchItem>  
+        Then Display searching results
+       
+        Examples:
+            | TestID     | searchItem    |
+            | DEMO_TC001 | Austin, TX    |
+            | DEMO_TC002 | Dallas, TX    |
+            | DEMO_TC003 | Arlington, TX |
+
+
+    Scenario Outline: Check if our system does not allow guests to book a single-night stay at all searched locations
+
+        Given Kasa page is opened
+        When Search with <searchItem>
+        Then Pick dates
+        Then Display searching results
+        Then Not allowed book property for single-night
 
         Examples:
-            | TestID     | SearchItem | ExpectedURL           |
-            | DEMO_TC001 | WDIO       | https://webdriver.io/ |
+            | TestID     | searchItem    |
+            | DEMO_TC004 | Austin, TX    |
+            | DEMO_TC005 | Dallas, TX    |
+            | DEMO_TC006 | Arlington, TX |
+
+
+    Scenario Outline: Bookings can be only made on a future date
+
+        Given Kasa page is opened
+        When Search with <searchItem>
+        Then Not allowed book on past date <pastDate>
+
+
+        Examples:
+            | TestID     | searchItem | pastDate |
+            | DEMO_TC007 | Austin, TX | 2022-01-12 |
+
+    @demo
+    Scenario Outline: Checks if a selected Kasa has "Heating" in the amenities list
+        Given Kasa page is opened
+        When Search with <searchItem>
+        Then Pick dates
+        Then Display searching results
+        Then Heating in the amenities
+
+        Examples:
+            | TestID     | searchItem    |
+            | DEMO_TC008 | Austin, TX    |
+            | DEMO_TC009 | Dallas, TX    |
+            | DEMO_TC010 | Arlington, TX |
--- test/features/step-definitions/demo.ts
+++ test/features/step-definitions/demo.ts
@@ -1,62 +1,107 @@
 import { Given, When, Then } from "@wdio/cucumber-framework";
 import chai from "chai"
 
-Given(/^Google page is opened$/, async function () {
-    browser.url('https://www.google.com')
-    //browser.debug()
-
+Given(/^Kasa page is opened$/, async function () {
+    await browser.url("https://www.kasa.com/")
+    await browser.pause(2000)
 
 })
 
 When(/^Search with (.*)$/, async function (searchItem) {
     console.log('>> searchItem: ${searchItem}');
-    let ele = await $('[name=q]');
-    await ele.setValue(searchItem);
-    await browser.keys("Enter")
+    let ele = await $('#full-screen-hero-search-input') // input field
 
+    await ele.setValue(searchItem) 
+})
+
+Then(/^Not allowed book on past date (.*)$/, async function (pastDate) {
+    let dateInEle = await $('#full-screen-hero-check-in-input') //check in intput field
+    await dateInEle.click()
+
+    await browser.pause(2000)
+    
+    let ele = await $("//button[@date='"+pastDate+"']");
+      
+    let val = await ele.getAttribute('disabled');
+    chai.expect(val).to.equal('true'); // make sure that element is disabled
 
 })
 
-Then(/^Click on the first search result$/, async function () {
-    let ele = await $('<h3>')
-    ele.click()
-}
-)
+Then(/^Pick dates$/, async function () {
+    var checkinDate = new Date(); // create Date obj for checkin
+    var ddIn = checkinDate.getDate() + 1; // incremented 1 day
+    var mmIn = checkinDate.getMonth() + 1;
+    var yyyyIn = checkinDate.getFullYear();
 
-Then(/^URL should match (.*)$/, async function (expectedURL) {
-    console.log('>> expectedURL: ${expectedURL}');
-    let url = await browser.getUrl()
-    chai.expect(url).to.equal(expectedURL)
+    var ddInString = ddIn.toString(); // converted number to string datatype
+    var mmInString = mmIn.toString(); // converted number to string datatype
 
-})
-
-// Web interactions
-Given(/^A web page is opened$/, async function () {
-    await browser.url('/inputs')
-    await browser.setTimeout({ implicit: 15000, pageLoad: 10000 })
-    await browser.maximizeWindow()
-
-})
-
-When(/^Perform web interactions$/, async function () {
-    /**
-     * input box
-     * Actions:
-     * 1. type into imput box
-     * 2. clear the field or add value
-     * 3. click and type
-     * 4. slow typing
-     */
-    let number = 12345
-    let strNum = number.toString()
-    let ele = await $('[type = number]')
-    await ele.click() //scrollinoview or moveto alternatives
-    // await ele.setValue(strNum)
-    for (let i = 0; i < strNum.length; i++) {
-        //await ele.setValue(strNum[i]) 
-        await browser.pause(1000)
-        await browser.keys(strNum[i])
+    if (ddIn < 10) { // formatting
+        ddInString = '0' + ddIn;
+    }
+    if (mmIn < 10) { // formatting
+        mmInString = '0' + mmIn;
     }
 
-    await browser.debug()
+    var dateIn = mmInString + '/' + ddInString + '/' + yyyyIn;
+    console.log(dateIn);
+    let dateInEle = await $('#full-screen-hero-check-in-input') // checkin intput field
+    await dateInEle.setValue(dateIn)
+
+    var checkoutDate = new Date(); // create Date obj for checkout
+    checkoutDate.setDate(checkinDate.getDate() + 2);
+    var ddOut = checkoutDate.getDate();
+    var mmOut = checkoutDate.getMonth() + 1;
+    var yyyyOut = checkoutDate.getFullYear();
+
+    var ddOutString = ddOut.toString(); // converted number to string datatype
+    var mmOutString = mmOut.toString(); // converted number to string datatype
+
+    if (ddOut < 10) {// formatting
+        ddOutString = '0' + ddOut;
+    }
+    if (mmOut < 10) {// formatting
+        mmOutString = '0' + mmOut;
+    }
+
+    var dateOut = mmOutString + '/' + ddOutString + '/' + yyyyOut;
+
+    let dateOutEle = await $('#full-screen-hero-check-out-input') // checkout intput field
+    await dateOutEle.setValue(dateOut)
+    await browser.pause(2000)
+
+})
+
+Then(/^Display searching results$/, async function () {
+    let ele = await $('//button[@type="submit"]') // submit element
+    await ele.click()
+    await browser.pause(2000)
+
+    let eleTitleLink = await $("//div[@class='property-card__header']//a") //title link element
+    await eleTitleLink.scrollIntoView()
+    await eleTitleLink.click()
+
+    let eleTitkeBut = await $("//div[@class='room-type-card__header']//button")//title button element
+    let eleH2 = await $("<h2>") //for scrolling
+
+    await eleH2.scrollIntoView()
+
+    await eleTitkeBut.click()
+    await browser.pause(2000)
+
+})
+
+Then(/^Heating in the amenities$/, async function () {
+
+    let ele = await $("//li[text()=' Heating ']")//li element
+    await ele.scrollIntoView()
+    await browser.pause(2000)
+})
+
+Then(/^Not allowed book property for single-night$/, async function () {
+
+    let ele = await $("//div[@class='availability-button-row']").isExisting()// element that holds "Book" button and price info
+    
+    chai.expect(ele).to.eql(false)//if ele does not exist (false), this test will pass
+    await browser.pause(2000)
 })
